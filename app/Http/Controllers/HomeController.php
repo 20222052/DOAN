69cc4887cb082488f6a203c18c32387841e7b31e
Thanh Tùng Nguyễn <20222052@eaut.edu.vn>
2024-08-01 17:16:01 +0700

    first commit

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,117 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    public function about()
+    {
+        return view('about');
+    }
+    public function contact()
+    {
+        return view('contact');
+    }
+    public function blog()
+    {
+        return view('blog');
+    }
+    public function cart()
+    {
+        return view('cart');
+    }
+    public function services()
+    {
+        return view('services');
+    }
+    public function shop()
+    {
+        return view('shop');
+    }
+    public function thankyou()
+    {
+        return view('thankyou');
+    }
+    public function acount()
+    {
+        return view('acount');
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('index');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Http\Controllers\HomeController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+//không cần controller mà return trực tiếp view
+// Route::get('/', function () {
+//     return view('contact');
+// });
+
+Route::get('/', [HomeController::class, 'index'])->name('index');
+Route::get('/about', [HomeController::class, 'about'])->name('about');
+Route::get('/contact', [HomeController::class, 'contact'])->name('contact');
+Route::get('/blog', [HomeController::class, 'blog'])->name('blog');
+Route::get('/cart', [HomeController::class, 'cart'])->name('cart');
+Route::get('/services', [HomeController::class, 'services'])->name('services');
+Route::get('/shop', [HomeController::class, 'shop'])->name('shop');
+Route::get('/thankyou', [HomeController::class, 'thankyou'])->name('thankyou');
+Route::get('/acount', [HomeController::class, 'acount'])->name('acount');
+
